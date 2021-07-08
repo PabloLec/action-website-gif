1bbccbf61d50350e5bc7d7d3db876437276fbeee
PabloLec <pablo.lecolinet@gmail.com>
2021-07-08 17:16:23 +0200

    to be tested

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -o errexit -o pipefail -o nounset
 
+pwd
+echo "______________________"
+ls
+echo "______________________"
+
 python3 /app/script.py || echo
 
-cat /app/geckodriver.log
+echo "______________________"
+
+cat geckodriver.log
 
 cp /app/image.gif /github/workspace/
